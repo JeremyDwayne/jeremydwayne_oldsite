92e139f69b9f9271d97d9403a76be1be19116dd8
Jeremy Winterberg <winterjd@uwec.edu>
2016-09-25 04:10:49 -0500

    session may be the issue with logging in?

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,6 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
-set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
