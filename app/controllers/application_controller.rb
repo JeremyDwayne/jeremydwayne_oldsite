833d00da04433211dc05314d04686128e38016f7
Jeremy Winterberg <winterjd@uwec.edu>
2016-09-25 04:33:15 -0500

    lkdfs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   include Pundit
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   protect_from_forgery prepend: true
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
     def user_not_authorized
