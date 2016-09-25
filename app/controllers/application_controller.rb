b0da0143bd0f0bf545adc92b3ef4686e9c39876b
Jeremy Winterberg <winterjd@uwec.edu>
2016-09-25 05:41:08 -0500

    one of these commits will be unlike the other, and FREAKING WORK

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-  protect_from_forgery with: :exception, prepend: true
+  protect_from_forgery with: :null_session
 
   private
     def user_not_authorized
