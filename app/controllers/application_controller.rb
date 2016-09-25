788ea9f98b828203a8f3c6523c12abb246e2b2e7
Jeremy Winterberg <winterjd@uwec.edu>
2016-09-25 04:18:11 -0500

    maybe?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-  protect_from_forgery
+  protect_from_forgery prepend: true
 
   private
     def user_not_authorized
