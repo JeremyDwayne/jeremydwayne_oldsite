ef9d933afc43e650b0b86173226eaa896f27580c
Jeremy Winterberg <winterjd@uwec.edu>
2016-09-25 02:59:33 -0500

    testing font removal if error still exists

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,8 +2,6 @@ set :stage, :production
 set :application, 'jeremydwayne' 
 set :repo_url, 'git@github.com:jeremydwayne/jeremydwayne.git'
 set :user, "deploy"
-set :puma_threads, [4, 16]
-set :puma_workers, 0
 # set :rails_env, "production"
 set :scm, :git
 set :branch, "master"
@@ -13,12 +11,10 @@ set :deploy_via, :remote_cache
 set :deploy_to, "/var/www/jeremydwayne"
 set :tmp_dir, "/home/deploy/tmp"
 # server "jeremydwayne.com", roles: [:app, :web, :db], :primary => true
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+
+set :puma_threads, [4, 16]
+set :puma_workers, 0
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
@@ -26,6 +22,11 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
   task :make_dirs do
