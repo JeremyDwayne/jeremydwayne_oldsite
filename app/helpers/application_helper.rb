58216d755db0713029e37c2c7ff5b00fec4187c2
Jeremy Winterberg <winterjd@uwec.edu>
2016-09-26 00:45:14 -0500

    changed from pygments to coderay

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,45 +1,45 @@
 module ApplicationHelper
-  def active_class(link_path) 
-    if current_page? link_path
-      "active"
-    elsif request.fullpath.include? link_path
-      if current_page? request.fullpath
-        if link_path =~ /\/.+/
-          "active"
-        end
-      end
-    end
-  end
+	def active_class(link_path) 
+		if current_page? link_path
+			"active"
+		elsif request.fullpath.include? link_path
+			if current_page? request.fullpath
+				if link_path =~ /\/.+/
+					"active"
+				end
+			end
+		end
+	end
 
-  class HTMLwithPygments < Redcarpet::Render::HTML
-    def block_code(code, language)
-      Pygments.highlight(code, lexer: language)
-    end
-  end
+	class CodeRayify < Redcarpet::Render::HTML
+		def block_code(code, language)
+			CodeRay.scan(code, language).div
+		end
+	end
 
-  def markdown(content)
-    renderer = HTMLwithPygments.new(hard_wrap: true, filter_html: true)
-    options = {
-      autolink: true,
-      no_intra_emphasis: true,
-      disable_indented_code_blocks: true,
-      fenced_code_blocks: true,
-      lax_html_blocks: true,
-      strikethrough: true,
-      superscript: true
-    }
+	def markdown(text)
+		coderayified = CodeRayify.new(:filter_html => true, 
+																	:hard_wrap => true)
+		options = {
+			:fenced_code_blocks => true,
+			:no_intra_emphasis => true,
+			:autolink => true,
+			:strikethrough => true,
+			:lax_html_blocks => true,
+			:superscript => true
+		}
+		markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+		markdown_to_html.render(text).html_safe
+	end
 
-    Redcarpet::Markdown.new(renderer, options).render(content).html_safe
-  end
+	def link_if_admin(text, path, css="")
+		if user_signed_in? && current_user.admin?
+			link_to text, path, class: css
+		end
+	end
 
-  def link_if_admin(text, path, css="")
-    if user_signed_in? && current_user.admin?
-        link_to text, path, class: css
-    end
-  end
-
-  def html_dash_replace(string)
-    string.gsub("-", "%")
-  end
+	def html_dash_replace(string)
+		string.gsub("-", "%")
+	end
 
 end
