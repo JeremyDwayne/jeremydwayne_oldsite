5d89b94f6982eccf296c323279137757427c338d
Jeremy Winterberg <winterjd@uwec.edu>
2017-10-08 01:36:21 -0500

    finally got coderay styled correctly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,13 +13,12 @@ module ApplicationHelper
 
   class CodeRayify < Redcarpet::Render::HTML
     def block_code(code, language)
-      CodeRay.scan(code, language).div
+      CodeRay.scan(code, language).div(line_numbers: :table, css: :class)
     end
   end
 
   def markdown(text)
-    coderayified = CodeRayify.new(:filter_html => true, 
-                                  :hard_wrap => true)
+    coderayified = CodeRayify.new(:filter_html => true, :hard_wrap => true)
     options = {
       :fenced_code_blocks => true,
       :no_intra_emphasis => true,
